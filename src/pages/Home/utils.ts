77aae08aea838da47eb772a17c5c7678359caac5
pablo <progeroffline@gmail.com>
2025-11-17 01:52:20 +0100

    feat(parsing date): Update function to new date type

--- src/pages/Home/utils.ts
+++ src/pages/Home/utils.ts
@@ -3,18 +3,18 @@
  * @param text - The URL or string to parse.
  * @returns A Date object if the pattern is found, otherwise null.
  * @example
- * // returns new Date(2024, 6, 25, 21, 55)
- * parseDateFromPath('/path/to/25-07-2024-21-55/video.mp4')
+ * // returns new Date(2025, 6, 25, 21, 55)
+ * parseDateFromPath('/path/to/2025/7/25/21-55/720/video.mp4')
  */
+
 function parseDateFromPath(text: string): Date | null {
-  // Regex to find a date in dd-mm-yyyy-hh-mm format.
-  const regex = /(\d{2})-(\d{2})-(\d{4})-(\d{2})-(\d{2})/;
+  // Regex to find a date in yyyy/m/d/hh-mm format.
+  const regex = /(\d{4})\/(\d{1,2})\/(\d{1,2})\/(\d{1,2})-(\d{1,2})/;
   const match = text.match(regex);
 
   if (!match) return null;
 
-  // The first element in match is the full string, so we skip it.
-  const [_, day, month, year, hour, minute] = match.map(Number);
+  const [_, year, month, day, hour, minute] = match.map(Number);
 
   // Note: The month in the Date constructor is 0-indexed (0-11).
   return new Date(year, month - 1, day, hour, minute);
